28b552f4e4d8579db512b501c6de79fa931df069
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-09-01 19:14:44 -0400

    "added App component, added server index and client index.js"

---
+++ public/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(){
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                THIS IS THE REACT APP COMPONENT
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import request from 'request';
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+app.use(express.static('css'));
+app.use(express.static('images'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/src', 'index.html'));
+})
+
+app.get('#', (req, res) => {
+    console.log('Pound hashtag works');
+})
+
+app.post('passphrase', (req, res) => {
+    console.log('passphrase tag works');
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/src', 'index.html'));
+})
